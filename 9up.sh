51a152005b100c26268cd4034920819aea949aee
Adam Anderson <84adam@users.noreply.github.com>
2018-09-04 16:12:47 -0700

    default to $HOME for downloads

--- 9up.sh
+++ 9up.sh
@@ -7,13 +7,13 @@
 # wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
 
 # patch 1 
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda_9.0.176.1_linux-run -O cudapatch_176_1.run
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda_9.0.176.1_linux-run -O ~/cudapatch_176_1.run
 
 # patch 2 
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda_9.0.176.2_linux-run -O cudapatch_176_2.run
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda_9.0.176.2_linux-run -O ~/cudapatch_176_2.run
 
 # patch 3 
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda_9.0.176.3_linux-run -O cudapatch_176_3.run
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda_9.0.176.3_linux-run -O ~/cudapatch_176_3.run
 
 # patch 4 
-wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda_9.0.176.4_linux-run -O cudapatch_176_4.run
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda_9.0.176.4_linux-run -O ~/cudapatch_176_4.run
