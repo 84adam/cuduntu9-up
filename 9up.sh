aab0219756e2deed77bbfae6d2edf1f5c64a7c94
Adam Anderson <84adam@users.noreply.github.com>
2018-09-04 15:57:28 -0700

    Create 9up.sh

---
+++ 9up.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# download CUDA 9.0 .run local patches for Ubuntu 16.04
+# see: https://developer.nvidia.com/cuda-90-download-archive?target_os=Linux&target_arch=x86_64&target_distro=Ubuntu&target_version=1604&target_type=runfilelocal
+
+# base installer
+# wget https://developer.nvidia.com/compute/cuda/9.0/Prod/local_installers/cuda_9.0.176_384.81_linux-run
+
+# patch 1 
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/1/cuda_9.0.176.1_linux-run -O cudapatch_176_1.run
+
+# patch 2 
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/2/cuda_9.0.176.2_linux-run -O cudapatch_176_2.run
+
+# patch 3 
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/3/cuda_9.0.176.3_linux-run -O cudapatch_176_3.run
+
+# patch 4 
+wget https://developer.nvidia.com/compute/cuda/9.0/Prod/patches/4/cuda_9.0.176.4_linux-run -O cudapatch_176_4.run
